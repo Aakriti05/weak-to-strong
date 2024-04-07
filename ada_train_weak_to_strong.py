e66a69bd180e6bdd1e9f266d0a8119f16d9b6bf0
Aakriti05 <juhi05aakritiagrawal@gmail.com>
2024-04-07 02:14:08 -0400

    small bug

--- ada_train_weak_to_strong.py
+++ ada_train_weak_to_strong.py
@@ -210,9 +210,9 @@ def main(
 
 
     if split_by_difficulty:
-        train1_ds = load_from_disk(ds_name + "/dataset_{weak_model_size}" + "/adaboost/train1_10000_0")
-        train2_ds = load_from_disk(ds_name + "/dataset_{weak_model_size}" + "/train2") #"./dataset/" + 
-        test_ds = load_from_disk(ds_name + "/dataset_{weak_model_size}" + "/test") #"./dataset/" +
+        train1_ds = load_from_disk(ds_name + "/" + weak_model_size + "/adaboost/train1_10000_0")
+        train2_ds = load_from_disk(ds_name + "/" + weak_model_size + "/train2") #"./dataset/" + 
+        test_ds = load_from_disk(ds_name + "/" + weak_model_size + "/test") #"./dataset/" +
 
     else:
         # Load dataset
@@ -292,7 +292,7 @@ def main(
 
     
     if adaboost:
-        weak_ds = load_from_disk(ds_name + "/" + weak_model_size + "/adaboost/weak_data_" + rounds "/".format()) #"./dataset/" + 
+        weak_ds = load_from_disk(ds_name + "/" + weak_model_size + "/adaboost/weak_data_" + str(rounds) + "/".format()) #"./dataset/" + 
 
         weak_accuracy = 1
     
