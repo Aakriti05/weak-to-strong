4f47838e7b42e0c62880a1c2895022b80f2bfb85
Aakriti05 <juhi05aakritiagrawal@gmail.com>
2024-04-13 15:59:50 -0400

    bug fix

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ conda activate w2s
 echo `which python`
 
 weak_model="gpt2"
-strong_model="gpt2-large"
+strong_model="gpt2-medium"
 ds_name="sciq"
 weighted_sampling=False
 weak_gt_epochs=3
@@ -20,34 +20,34 @@ start_round=1
 rounds=10
 adaboost=True
 
-# [ -d "./${ds_name}" ] && mv "./${ds_name}" "./${ds_name}_data" || echo "Directory does not exist, skipping move."
+[ -d "./${ds_name}" ] && mv "./${ds_name}" "./${ds_name}_data" || echo "Directory does not exist, skipping move."
 
-# directory="./results/results_${weak_model}_to_${strong_model}"
+directory="./results/results_${weak_model}_to_${strong_model}"
 
-# [ -d "$directory" ] && rm -r "$directory" && echo "Directory '$directory' has been deleted." || echo "Directory '$directory' does not exist."
-
-
-# python "./ada_train_weak.py" --split_by_difficulty $split_by_difficulty --split_by_random $split_by_random --loss_ $loss --weak_model_size $weak_model --ds_name $ds_name --weighted_sampling $weighted_sampling --gt_epochs $weak_gt_epochs --results_folder "./results/results_${weak_model}_to_${strong_model}"
-# for ((Epoch=1; Epoch<=rounds; Epoch++));
-# do
-#     for filename in "./ada_generate_weight.py" "ada_train_weak_weight.py"
-#     do
-#         echo $filename
-#         echo $Epoch
-#         python $filename --E $Epoch --weak_model_size $weak_model --ds_name $ds_name --weighted_sampling $weighted_sampling --gt_epochs $weak_gt_epochs --results_folder "./results/results_${weak_model}_to_${strong_model}"
-#     done
-# done
-# python ada_predict.py --weak_model_size $weak_model --rounds $rounds --ds_name $ds_name --w2s_generalisation $w2s_generalisation --results_folder "./results/results_${weak_model}_to_${strong_model}"
-# rm -rf ./${ds_name}/${weak_model}/adaboost/weak_model_${rounds}
+[ -d "$directory" ] && rm -r "$directory" && echo "Directory '$directory' has been deleted." || echo "Directory '$directory' does not exist."
 
 
+python "./ada_train_weak.py" --split_by_difficulty $split_by_difficulty --split_by_random $split_by_random --loss_ $loss --weak_model_size $weak_model --ds_name $ds_name --weighted_sampling $weighted_sampling --gt_epochs $weak_gt_epochs --results_folder "./results/results_${weak_model}_to_${strong_model}"
+for ((Epoch=1; Epoch<=rounds; Epoch++));
+do
+    for filename in "./ada_generate_weight.py" "ada_train_weak_weight.py"
+    do
+        echo $filename
+        echo $Epoch
+        python $filename --E $Epoch --weak_model_size $weak_model --ds_name $ds_name --weighted_sampling $weighted_sampling --gt_epochs $weak_gt_epochs --results_folder "./results/results_${weak_model}_to_${strong_model}"
+    done
+done
+python ada_predict.py --weak_model_size $weak_model --rounds $rounds --ds_name $ds_name --w2s_generalisation $w2s_generalisation --results_folder "./results/results_${weak_model}_to_${strong_model}"
+rm -rf ./${ds_name}/${weak_model}/adaboost/weak_model_${rounds}
 
 
-# echo "Training Baseline weak to strong !!!!!!!!!!!!!"
-# directory="./results/results_${weak_model}_to_${strong_model}/w2s_default"
-# [ -d "$directory" ] && rm -r "$directory" && echo "Directory '$directory' has been deleted." || echo "Directory '$directory' does not exist."
 
-# python ada_train_weak_to_strong.py --gt_epochs strong_gt_epochs --weak_model_size $weak_model --adaboost False --rounds $round --strong_model_size $strong_model --results_folder "./results/results_${weak_model}_to_${strong_model}" --sweep_subfolder "./w2s_default"
+
+echo "Training Baseline weak to strong !!!!!!!!!!!!!"
+directory="./results/results_${weak_model}_to_${strong_model}/w2s_default"
+[ -d "$directory" ] && rm -r "$directory" && echo "Directory '$directory' has been deleted." || echo "Directory '$directory' does not exist."
+
+python ada_train_weak_to_strong.py --weak_gt_epochs $weak_gt_epochs --gt_epochs $strong_gt_epochs --weak_model_size $weak_model --adaboost False --rounds $round --strong_model_size $strong_model --results_folder "./results/results_${weak_model}_to_${strong_model}" --sweep_subfolder "./w2s_default"
 
 
 
